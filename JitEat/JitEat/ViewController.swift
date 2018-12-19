a3f80ad9d59eb538ef66b292a1800cec9064ef56
mprzybolewski <mprzybolewski@gmail.com>
2018-12-19 19:46:01 +0100

    add base project

---
+++ JitEat/JitEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JitEat
+//
+//  Created by Marek Przybolewski on 19/12/2018.
+//  Copyright © 2018 Marek Przybolewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
