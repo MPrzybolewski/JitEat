f0b8fc9289adc602cc51080143a279370198a721
mprzybolewski <mprzybolewski@gmail.com>
2018-12-19 19:59:26 +0100

    change viewcontroller name and add loginViewController

---
+++ JitEat/JitEat/ViewControllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  JitEat
+//
+//  Created by Marek Przybolewski on 19/12/2018.
+//  Copyright © 2018 Marek Przybolewski. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
